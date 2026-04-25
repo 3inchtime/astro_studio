4626ab0a9a4e83f7afbf3dd991766d7c102b79de
3inchtime <3inchtime@gmail.com>
2026-04-26 00:47:52 +0800

    feat: add /favorites route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AppLayout from "./components/layout/AppLayout";
 import GeneratePage from "./pages/GeneratePage";
 import GalleryPage from "./pages/GalleryPage";
+import FavoritesPage from "./pages/FavoritesPage";
 import SettingsPage from "./pages/SettingsPage";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route element={<AppLayout />}>
           <Route path="/generate" element={<GeneratePage />} />
           <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
           <Route path="/settings" element={<SettingsPage />} />
           <Route path="*" element={<Navigate to="/generate" replace />} />
         </Route>
